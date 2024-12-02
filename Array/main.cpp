b9fd3cdce8b191f7bd2d7a9fd3fe49928d0eede5
Eiadgamal <eyadcataract20@gmail.com>
2024-12-02 03:28:22 +0200

    Last Modify

--- Array/main.cpp
+++ Array/main.cpp
@@ -16,55 +16,52 @@ int main()
     myarray.getSize(); 
     myarray.display();
 
-    // cout << "Enter Key to Search for  " << endl;
-    // int index;
-    // cin >> index;
-    // myarray.search(index);
+    cout << "Enter Key to Search for  " << endl;
+    int index;
+    cin >> index;
+    myarray.search(index);
 
-    // cout << "Do You Want to Add item ? (yes / no)" << endl;
-    // string ask;
-    // cin >> ask;
-    // cout << "Enter Value you want to append " << endl;
-    // int value;
-    // cin >> value;    
-    // if (ask == "yes") {
-    //     myarray.append(value);
-    // }
-    // else
-    // {
-    //     cout << "Okay , As You Like :) " << endl;
-    // }
-    // myarray.getSize(); 
-    // myarray.display();
+    cout << "Do You Want to Add item ? (yes / no)" << endl;
+    string ask;
+    cin >> ask;
+    cout << "Enter Value you want to append " << endl;
+    int value;
+    cin >> value;    
+    if (ask == "yes") {
+        myarray.append(value);
+    }
+    else
+    {
+        cout << "Okay , As You Like :) " << endl;
+    }
+    myarray.getSize(); 
+    myarray.display();
 
 
-    // cout << "Enter Index you want to insert " << endl;
-    // int idx , idxValue ;
-    // cin >> idx ;
-    // cout << "Enter Value you want to insert " << endl;    
-    // cin >> idxValue; 
-    // myarray.insert(idx,idxValue);
-    // myarray.display();
+    cout << "Enter Index you want to insert " << endl;
+    int idx , idxValue ;
+    cin >> idx ;
+    cout << "Enter Value you want to insert " << endl;    
+    cin >> idxValue; 
+    myarray.insert(idx,idxValue);
+    myarray.display();
     
-    // cout << "Enter Index you want to delete " << endl;
-    // int idx ;
-    // cin >> idx ;
-    // myarray.Delete(idx);
-    // myarray.display();
+    cout << "Enter Index you want to delete " << endl;
+    int idx ;
+    cin >> idx ;
+    myarray.Delete(idx);
+    myarray.display();
 
-    // cout << "Enter newsize " << endl;
-    // int newsize ;
-    // cin >> newsize ;
-    // myarray.Enlarge(newsize);
-    // myarray.display();
+    cout << "Enter newsize " << endl;
+    int newsize ;
+    cin >> newsize ;
+    myarray.Enlarge(newsize);
+    myarray.display();
 
     Array other(6);
     other.fill();
     myarray.Merge(other);
     cout << "Array Size is : " << myarray.getSize() << " While Array Length is : " << myarray.getLength() << endl;    
     myarray.display();
-
-
-
 
 };
